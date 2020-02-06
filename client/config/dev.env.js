15256e383daa0b5636b3a609a83bb43a9a9a683b
daeun.kim <daeun.kim@lge.com>
2020-02-06 13:42:37 +0900

    AWS URL 변경

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  LCORD_API_Url: '"http://52.79.217.247:3000"',
+  LCORD_API_Url: '"http://13.125.18.161:3000"',
 })
