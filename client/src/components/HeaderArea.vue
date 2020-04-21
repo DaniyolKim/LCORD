17bf86765f846d3a4fb49413cb319d488d0abcd7
daeun.kim <daeun.kim@lge.com>
2020-04-21 12:47:18 +0900

    Header area : text 잘 못 표현 되는 것 수정

--- client/src/components/HeaderArea.vue
+++ client/src/components/HeaderArea.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="header-root">
-    <div v-if="userId != ''">
-    <!--<div v-if="userId != null">-->
+    <div v-if="userId === undefined">
+      <router-link :to="{ name: 'Login'}" >Sign in<i class="fas fa-sign-in-alt"></i></router-link>
+    </div>
+    <div v-else>
       <label>안녕하세요. <router-link :to="{ name: 'MyInfo'}" style="color: white">{{userId}}</router-link>님</label>
       <router-link :to="{ name: 'Login'}"><i class="fas fa-sign-out-alt"></i></router-link>
     </div>
-    <router-link v-if="userId == ''" :to="{ name: 'Login'}" >Sign in<i class="fas fa-sign-in-alt"></i></router-link>
   </div>
 </template>
 
